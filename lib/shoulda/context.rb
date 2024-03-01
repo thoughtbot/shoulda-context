4ffa62cf8ecd3453f7d3cf48bc5b6c928f664efa
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-03-01 19:37:18 +0100

    Fix a method redefinition warning
    The fix in rails was added somewhere in 5.X, #82 dropped support for rails 5.2, so just drop the workaround as well.
    
    ```
    /usr/local/bundle/gems/shoulda-context-2.0.0/lib/shoulda/context/rails_test_unit_reporter_patch.rb:8: warning: method redefined; discarding old format_rerun_snippet
    /usr/local/bundle/gems/railties-7.1.3/lib/rails/test_unit/reporter.rb:76: warning: previous definition of format_rerun_snippet was here
    ```

--- lib/shoulda/context.rb
+++ lib/shoulda/context.rb
@@ -9,7 +9,6 @@ require "shoulda/context/world"
 
 if defined?(Rails)
   require "shoulda/context/railtie"
-  require "shoulda/context/rails_test_unit_reporter_patch"
 end
 
 Shoulda::Context.configure do |config|
